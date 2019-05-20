82395fc3a27df6e712b5441e305a280f56e622a5
Riccardo Santoni <rsantoni@andrew.cmu.edu>
2019-05-20 16:31:10 -0400

    Main file creation and deletion of new device

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ def get_os():
 def check_os(opp):
     oss = manager.list_operating_systems()
     for os in oss:
-        if str(os) == opp:
+        os_string = str(os)
+        split = os_string.split()
+        #ewr1print(split)
+        if split[0] == opp:
             return True
     return False
 
@@ -34,12 +37,39 @@ def check_facility(fac):
 # Creates a new device on the specifications of the user
 def create_dev(host, fac, os):
     device = manager.create_device(project_id='ca73364c-6023-4935-9137-2132e73c20b4',
-                               hostname='host',
-                               plan='baremetal_1', facility='fac',
-                               operating_system='os')
+                               hostname= host,
+                               plan='baremetal_1', facility= fac,
+                               operating_system= os)
+    return device
 
 # Lists all the current devices of the project
 def list_dev():
     devices = manager.list_devices(project_id='ca73364c-6023-4935-9137-2132e73c20b4')
     for device in devices:
         print(device)
+        device.update()
+        device.delete()
+
+
+# Main function where user can decide what to do
+if __name__ == '__main__':
+    print("What operating system do you want to use? Possible Options:")
+    get_os()
+    os = input("")
+    # Checks to ensure that the operating system is valid
+    while not check_os(os) :
+        os = input("Invalid operating system, please enter a valid option:")
+
+    print("What facility would you like?")
+    get_facilities()
+    facility = input("")
+    # Checks to ensure that the facility is valid
+    while not check_facility(facility):
+        facility = input("Invalid facility, please enter a valid option:")
+
+    hostname = input("What would you like the hostname to be?")
+
+    # Creates a new device based on the inputted information
+    device = create_dev(hostname, facility, os)
+    print(device.id)
+    list_dev()
