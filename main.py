9e559f46682de22bbdb030a30a4f8438d88094b2
Riccardo Santoni <rsantoni@andrew.cmu.edu>
2019-05-20 14:08:54 -0400

    Added more helper functions

--- main.py
+++ main.py
@@ -13,3 +13,27 @@ def check_os(opp):
         if str(os) == opp:
             return True
     return False
+
+def get_facilities():
+    facilities = manager.list_facilities()
+    for facility in facilities:
+        print(facility)
+
+def check_facility(fac):
+    facilities = manager.list_facilities()
+    for facility in facilities:
+        if str(facility) == fac:
+            return True
+    return False
+
+def create_dev(host, fac, os):
+    device = manager.create_device(project_id='ca73364c-6023-4935-9137-2132e73c20b4',
+                               hostname='host',
+                               plan='baremetal_1', facility='fac',
+                               operating_system='os')
+
+
+def list_dev():
+    devices = manager.list_devices(project_id='ca73364c-6023-4935-9137-2132e73c20b4')
+    for device in devices:
+        print(device)
